82f525f0d46197e18d66730688807b36135abd07
Christopher Laut <chriss.laut@gmail.com>
2018-02-06 12:15:30 +0100

    Textures and Lang

--- src/main/java/de/chriss199815/chrisstweak/block/ModBlocks.java
+++ src/main/java/de/chriss199815/chrisstweak/block/ModBlocks.java
@@ -10,41 +10,49 @@ public class ModBlocks {
 
 
     //Compressed Cobblestone
-    public static BlockBase compCobble = new BlockNorm("comp_cobblestone").setCreativeTab(CreativeTabs.MATERIALS);
-    public static BlockBase compCobble2 = new BlockNorm("comp_cobblestone2").setCreativeTab(CreativeTabs.MATERIALS);
-    public static BlockBase compCobble3 = new BlockNorm("comp_cobblestone3").setCreativeTab(CreativeTabs.MATERIALS);
-    public static BlockBase compCobble4 = new BlockNorm("comp_cobblestone4").setCreativeTab(CreativeTabs.MATERIALS);
-    public static BlockBase compCobble5 = new BlockNorm("comp_cobblestone5").setCreativeTab(CreativeTabs.MATERIALS);
-    public static BlockOre oreCopper = new BlockOre("ore_copper").setCreativeTab(CreativeTabs.MATERIALS);
+    public static BlockOre oreCobble1 = new BlockOre("ore_cobblestone1").setCreativeTab(CreativeTabs.MATERIALS);
+    public static BlockOre oreCobble2 = new BlockOre("ore_cobblestone2").setCreativeTab(CreativeTabs.MATERIALS);
+    public static BlockOre oreCobble3 = new BlockOre("ore_cobblestone3").setCreativeTab(CreativeTabs.MATERIALS);
+    public static BlockOre oreCobble4 = new BlockOre("ore_cobblestone4").setCreativeTab(CreativeTabs.MATERIALS);
+    public static BlockOre oreCobble5 = new BlockOre("ore_cobblestone5").setCreativeTab(CreativeTabs.MATERIALS);
+    public static BlockOre oreCobble6 = new BlockOre("ore_cobblestone6").setCreativeTab(CreativeTabs.MATERIALS);
+    public static BlockOre oreCobble7 = new BlockOre("ore_cobblestone7").setCreativeTab(CreativeTabs.MATERIALS);
+    public static BlockOre oreCobble8 = new BlockOre("ore_cobblestone8").setCreativeTab(CreativeTabs.MATERIALS);
 
     public static void register(IForgeRegistry<Block> registry){
             registry.registerAll(
-                    compCobble,
-                    compCobble2,
-                    compCobble3,
-                    compCobble4,
-                    compCobble5,
-                    oreCopper
+                    oreCobble1,
+                    oreCobble2,
+                    oreCobble3,
+                    oreCobble4,
+                    oreCobble5,
+                    oreCobble6,
+                    oreCobble7,
+                    oreCobble8
             );
     }
 
     public static void registerItemBlocks(IForgeRegistry<Item> registry){
             registry.registerAll(
-                    compCobble.createItemBlock(),
-                    compCobble2.createItemBlock(),
-                    compCobble3.createItemBlock(),
-                    compCobble4.createItemBlock(),
-                    compCobble5.createItemBlock(),
-                    oreCopper.createItemBlock()
+                    oreCobble1.createItemBlock(),
+                    oreCobble2.createItemBlock(),
+                    oreCobble3.createItemBlock(),
+                    oreCobble4.createItemBlock(),
+                    oreCobble5.createItemBlock(),
+                    oreCobble6.createItemBlock(),
+                    oreCobble7.createItemBlock(),
+                    oreCobble8.createItemBlock()
             );
     }
 
     public static void registerModuels(){
-            compCobble.registerItemModel(Item.getItemFromBlock(compCobble));
-            compCobble2.registerItemModel(Item.getItemFromBlock(compCobble2));
-            compCobble3.registerItemModel(Item.getItemFromBlock(compCobble3));
-            compCobble4.registerItemModel(Item.getItemFromBlock(compCobble4));
-            compCobble5.registerItemModel(Item.getItemFromBlock(compCobble5));
-            oreCopper.registerItemModel(Item.getItemFromBlock(oreCopper));
+        oreCobble1.registerItemModel(Item.getItemFromBlock(oreCobble1));
+        oreCobble2.registerItemModel(Item.getItemFromBlock(oreCobble2));
+        oreCobble3.registerItemModel(Item.getItemFromBlock(oreCobble3));
+        oreCobble4.registerItemModel(Item.getItemFromBlock(oreCobble4));
+        oreCobble5.registerItemModel(Item.getItemFromBlock(oreCobble5));
+        oreCobble6.registerItemModel(Item.getItemFromBlock(oreCobble6));
+        oreCobble7.registerItemModel(Item.getItemFromBlock(oreCobble7));
+        oreCobble8.registerItemModel(Item.getItemFromBlock(oreCobble8));
     }
 }
